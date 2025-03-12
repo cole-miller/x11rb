8468ddf28315d92e863984628657f7a87a7dd9f9
Your Name <you@example.com>
2025-03-12 09:53:01 -0400

    lskdjf

--- x11rb/src/protocol/dri3.rs
+++ x11rb/src/protocol/dri3.rs
@@ -17,10 +17,6 @@ where
         major_version,
         minor_version,
     };
-    let (bytes, fds) = request0.serialize(major_opcode(conn)?);
-    let slices = [IoSlice::new(&bytes[0])];
-    assert_eq!(slices.len(), bytes.len());
-    conn.send_request_with_reply(&slices, fds)
 }
 
 pub fn open<Conn>(
